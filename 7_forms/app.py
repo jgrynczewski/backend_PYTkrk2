2a3697ad388503f05d13f165355da4fc0dbd33d0
Jerzy <jgrynczewski@gmail.com>
2020-05-10 15:01:03 +0200

    Forms

---
+++ 7_forms/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/hello", methods=["POST"])
+def hello():
+    name = request.form.get("moje_imie")
+    return render_template("hello.html", name = name)
