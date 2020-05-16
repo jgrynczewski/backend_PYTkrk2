bb5ae6f904406b69c9bc53fd9de05cf13c568aa6
Jerzy <jgrynczewski@gmail.com>
2020-05-16 12:28:17 +0200

    Bootstrap

---
+++ 11_task_bootstrap/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        task = request.form.get("task")
+        tasks.append(task)
+    return render_template("index.html", tasks=tasks)
